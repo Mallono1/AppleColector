2c59ddac9e4582ad13bc178233b02bc65a9fa733
Marian Bak <mlazik1@gmail.com>
2024-11-30 01:55:38 +0100

    added game over and restart button

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const gameArea = document.getElementById("gameArea");
 const scoreDisplay = document.getElementById("score");
 const heartsDisplay = document.getElementById("hearts");
 const basket = document.getElementById("basket");
-const targetFps = 60;   
+const targetFps = 60;
+let gameOverElement = document.getElementById("gameOver");
+let restarbuttonElement = document.getElementById("restart-button");
 let appleSpeed = 100;
 const appleSpeedGain = 5;
 let lastAppleSpawnTime = Date.now();
 let appleSpawnTime = 1000;
 const appleSpawnTimeGain = 10;
 const maxAppleSpawnTime = 300;
-let hearts = 5;
+let hearts = 3;
 
 document.addEventListener("mousemove", (event) => {
   const basketRect = basket.getBoundingClientRect();
   const mouseX = event.clientX - gameArea.getBoundingClientRect().left;
-  basket.style.left = Math.min(
+  basket.style.left =
+    Math.min(
       Math.max(mouseX, basketRect.width / 2),
-      gameArea.clientWidth - basketRect.width/2
+      gameArea.clientWidth - basketRect.width / 2
     ) + "px";
 });
 
@@ -31,27 +34,31 @@ function createApple() {
   fallApple(apple);
   lastAppleSpawnTime = Date.now();
 }
+function gameOver() {
+  window.location.href = "endgame.html";
+}
 
 function fallApple(apple) {
   let lastUpdate = Date.now();
   let appleFallInterval = setInterval(() => {
     const appleRect = apple.getBoundingClientRect();
     const basketRect = basket.getBoundingClientRect();
-    const distanceDelta = (Date.now() - lastUpdate) / 1000 * appleSpeed;     
-    
+    const distanceDelta = ((Date.now() - lastUpdate) / 1000) * appleSpeed;
+
     if (appleRect.bottom >= gameArea.getBoundingClientRect().bottom) {
       clearInterval(appleFallInterval);
       apple.remove();
       hearts--;
-      heartsDisplay.textContent = '♥ '.repeat(hearts).trim();
-      if (hearts <= 0)
-      {
+      heartsDisplay.textContent = "♥ ".repeat(hearts).trim();
+      if (hearts <= 0) {
         //TODO gameover
+        //string.link("endgame.html");
+        gameOverElement.style.display = "flex";
+        restarbuttonElement.style.display = "flex";
         appleSpeed = 0;
         appleSpawnTime = Number.POSITIVE_INFINITY;
       }
     } else {
-
       apple.style.top = apple.offsetTop + distanceDelta + "px";
     }
 
@@ -65,7 +72,10 @@ function fallApple(apple) {
       score++;
       scoreDisplay.textContent = "Score: " + score;
       appleSpeed = appleSpeed + appleSpeedGain;
-      appleSpawnTime = Math.max(appleSpawnTime - appleSpawnTimeGain, maxAppleSpawnTime);
+      appleSpawnTime = Math.max(
+        appleSpawnTime - appleSpawnTimeGain,
+        maxAppleSpawnTime
+      );
     }
 
     lastUpdate = Date.now();
